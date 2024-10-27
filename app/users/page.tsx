25fd74d450bf2e276b14c805a1ab16aea0b40b74
NecipAkgz <nexco@hotmail.com>
2024-10-28 02:45:30 +0300

    feat: fetch and display user list with links
    Enhanced the Users page to asynchronously fetch user data from an API, improving the user experience by presenting a dynamically generated list of users. Each user name is now a clickable link for detailed views, making navigation more intuitive. Implemented revalidation to ensure fresh data regularly, addressing potential staleness in user information.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,28 @@
-const UsersPage = () => {
-  return <div>UsersPage</div>
+import Link from 'next/link'
+
+type User = {
+  id: number
+  name: string
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    next: { revalidate: 10 },
+  })
+  const users: User[] = await res.json()
+
+  return (
+    <>
+      <h1 className='text-2xl uppercase'>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 export default UsersPage
