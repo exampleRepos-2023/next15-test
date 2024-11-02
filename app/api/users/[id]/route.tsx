c74436549593e488cd0b0ca44de3bde299ec5e2f
NecipAkgz <nexco@hotmail.com>
2024-11-03 02:23:11 +0300

    feat(api): enhance user management with database interactions
    Updated user routes to replace hardcoded responses with dynamic data fetching from the database, allowing for more accurate user data retrieval and management. Improved validation by adding email checks and ensured consistent handling of user operations such as retrieval, update, and deletion.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,27 +1,29 @@
+import prisma from '@/prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 import schema from '../schema'
 
 interface Props {
   params: {
-    id: number
+    id: string
   }
 }
 
-export function GET(request: NextRequest, { params }: Props) {
-  if (params.id > 10) {
+export async function GET(request: NextRequest, { params }: Props) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  })
+
+  if (!user) {
     return NextResponse.json({ error: 'User not found' }, { status: 404 })
   }
 
-  return NextResponse.json({
-    id: params.id,
-    name: 'John Doe',
-    email: 'P6JpO@example.com',
-  })
+  return NextResponse.json(user)
 }
 
 export async function UPDATE(request: NextRequest, { params }: Props) {
   const body = await request.json()
-
   const validation = schema.safeParse(body)
 
   if (!validation.success) {
@@ -31,25 +33,45 @@ export async function UPDATE(request: NextRequest, { params }: Props) {
     )
   }
 
-  if (params.id > 10) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  })
+
+  if (!user) {
     return NextResponse.json({ error: 'User not found' }, { status: 404 })
   }
 
-  return NextResponse.json({
-    id: params.id,
-    name: 'John Doe',
-    email: 'P6JpO@example.com',
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: parseInt(params.id),
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
   })
+
+  return NextResponse.json(updatedUser)
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
-  if (params.id > 10) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  })
+
+  if (!user) {
     return NextResponse.json({ error: 'User not found' }, { status: 404 })
   }
 
-  return NextResponse.json({
-    id: params.id,
-    name: 'John Doe',
-    email: 'P6JpO@example.com',
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id: parseInt(params.id),
+    },
   })
+
+  return NextResponse.json(deletedUser)
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,19 +1,11 @@
+import prisma from '@/prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 import schema from './schema'
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'P6JpO@example.com',
-    },
-    {
-      id: 2,
-      name: 'Jane Doe',
-      email: 'LHkK5@example.com',
-    },
-  ])
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany()
+
+  return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
@@ -28,5 +20,22 @@ export async function POST(request: NextRequest) {
     )
   }
 
-  return NextResponse.json(body)
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  })
+
+  if (user) {
+    return NextResponse.json({ error: 'User already exists' }, { status: 409 })
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  })
+
+  return NextResponse.json(newUser, { status: 201 })
 }
