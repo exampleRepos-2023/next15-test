a255a7077755ea19aea320a22ca6144b186a142b
NecipAkgz <nexco@hotmail.com>
2024-11-04 01:59:13 +0300

    feat(auth): implement Google OAuth login and environment variable setup
    This update introduces Google authentication via NextAuth, enhancing user login capabilities, and adds a configuration file for environment variables to streamline the setup process for database and authentication settings.

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='flex bg-gray-950 p-4'>
+    <div className='flex space-x-4 bg-gray-950 p-4'>
       <Link className='mr-5' href='/'>
         Next.js
       </Link>
       <Link href='/users'>Users</Link>
+      <Link href='/api/auth/signin'>Login</Link>
     </div>
   )
 }
