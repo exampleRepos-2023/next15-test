3b27d5d3d3dc752d12e6d62ee6093a8ec31ffc73
NecipAkgz <nexco@hotmail.com>
2024-11-04 05:01:57 +0300

    feat(navbar): add user logout option in authenticated state and refine middleware protection
    The Navbar now displays a logout link for authenticated users, enhancing user experience by providing a clear option to sign out. Additionally, middleware has been introduced to protect user routes, ensuring that only authenticated users can access certain areas of the application.

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -12,7 +12,15 @@ const Navbar = () => {
       </Link>
       <Link href='/users'>Users</Link>
       {status === 'loading' && <div>Loading...</div>}
-      {status === 'authenticated' && <div>{session?.user?.name}</div>}
+      {status === 'authenticated' && (
+        <div>
+          {session?.user?.name}
+
+          <Link href='/api/auth/signout' className='ml-3'>
+            Logout
+          </Link>
+        </div>
+      )}
       {status === 'unauthenticated' && (
         <Link href='/api/auth/signin'>Login</Link>
       )}
